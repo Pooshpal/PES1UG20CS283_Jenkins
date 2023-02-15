f87ab44726ef0f6eb9f278629d016ee99127775f
Pooshpal <pooshpalbaheti@gmail.com>
2023-02-15 11:00:31 +0530

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "I have successfully built and run\n";
+  cout<<"I am making change to society by sleeping and eating\n";
+  return 0;
+}
